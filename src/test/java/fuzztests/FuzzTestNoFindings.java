925a2681f32792c6c5e17376b517445250e8af7e
Simon Resch <simon.resch@code-intelligence.de>
2024-03-27 14:34:37 +0100

    add 100 templated fuzz tests

---
+++ src/test/java/fuzztests/FuzzTestNoFindings.java
@@ -0,0 +1,61 @@
+package fuzztests;
+
+import com.code_intelligence.jazzer.junit.FuzzTest;
+
+import com.google.json.EvalMinifier;
+
+// @FuzzTest public void testID(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+
+public class FuzzTestNoFindings {
+  @FuzzTest public void test0(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test1(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test2(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test3(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test4(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test5(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test6(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test7(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test8(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test9(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test10(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test11(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test12(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test13(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test14(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test15(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test16(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test17(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test18(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test19(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test20(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test21(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test22(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test23(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test24(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test25(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test26(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test27(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test28(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test29(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test30(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test31(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test32(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test33(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test34(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test35(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test36(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test37(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test38(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test39(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test40(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test41(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test42(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test43(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test44(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test45(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test46(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test47(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test48(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+@FuzzTest public void test49(String input) { try { EvalMinifier.minify(input); } catch (Exception _e) {} }
+
+}
---
+++ src/test/java/fuzztests/FuzzTestWithFindings.java
@@ -0,0 +1,61 @@
+package fuzztests;
+
+import com.code_intelligence.jazzer.junit.FuzzTest;
+
+import com.google.json.EvalMinifier;
+
+// @FuzzTest public void testID(String input) { EvalMinifier.minify(input); }
+
+public class FuzzTestWithFindings {
+  @FuzzTest public void test0(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test1(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test2(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test3(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test4(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test5(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test6(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test7(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test8(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test9(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test10(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test11(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test12(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test13(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test14(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test15(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test16(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test17(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test18(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test19(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test20(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test21(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test22(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test23(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test24(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test25(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test26(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test27(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test28(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test29(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test30(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test31(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test32(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test33(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test34(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test35(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test36(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test37(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test38(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test39(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test40(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test41(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test42(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test43(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test44(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test45(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test46(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test47(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test48(String input) { EvalMinifier.minify(input); }
+@FuzzTest public void test49(String input) { EvalMinifier.minify(input); }
+
+}
